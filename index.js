d6d2fe4a37484166729557c0e5c7910708f4104d
danfin09 <dankfin@gmail.com>
2025-09-24 17:56:48 +0200

    trivia

--- index.js
+++ index.js
@@ -18,15 +18,30 @@ const questions = JSON.parse(fs.readFileSync('./questions.json', 'utf-8'));
 
 // Endpoint para obtener una pregunta aleatoria (con filtro por categoría)
 app.get('/api/question', (req, res) => {
-  res.send('Falta implementar el endpoint /api/question');
+  
+try{
+  const randomIndex = Math.floor(Math.random() * questions.length);
+  const randomQuestion = questions[randomIndex];
+
+  res.json(randomQuestion);
+} catch (error) {
+    console.error('Error al obtener la pregunta:', error);
+    res.status(500).json({ error: 'Error al obtener la pregunta' });
+  }
 });
 
 // Endpoint para obtener categorías únicas
 app.get('/api/categories', (req, res) => {
-  res.send('Falta implementar el endpoint /api/categories');
+ try {
+  const categories = [...new set(questions.map(q=>q.category))];
+  res.json(categories);
+ } catch (error) {
+  console.log("error al obtener categories", error);
+  res.status(500).json({error:"error interno en el selvidor"})
+ }
 });
 
 // Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor Trivia escuchando en http://localhost:${PORT}`);
-});
+})
